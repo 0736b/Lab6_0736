bdbe6165693d35487098b7a4be84dc8d329bfbf3
lab314-69 <lab314-69@labeng.local>
2020-12-25 16:12:47 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,18 +1,32 @@
-//Dialog of the first example is given below.
+#include<iostream>
+#include<string>
+using namespace std;
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker is a really cool name.
-Fahsai: I think you are an Engineering student. What is your student ID?
-Luke Skywalker: 590610999
-Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
-Fahsai: Let's go to the cinema together!!!
-Fahsai: What movie do you want to watch?
-Luke Skywalker: Star Wars VII
-Fahsai: So....which day are you free to go with me?
-Luke Skywalker: Next Monday
-Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars VII with you.
-Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
-*/
+int main(){
+string Name_ans,id,movie,date,goodbyetext;
+int year,gear,i=0;
+
+cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+cout << "?????: ";
+getline(cin,Name_ans);
+cout << "Fahsai: Wow!!! " << Name_ans << " is a really cool name." << endl;
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
+cout << Name_ans << ": ";
+cin >> year;
+cin.ignore();
+gear = (year/10000000) - 12;
+cout << "Fahsai: I think you may be GEAR "<< gear <<". I have a free movie ticket for you."<< endl;
+cout << "Fahsai: Let's go to the cinema together!!!"<< endl;
+cout << "Fahsai: What movie do you want to watch?" << endl;
+cout << Name_ans << ": ";
+getline(cin,movie);
+cout << "Fahsai: So....which day are you free to go with me?" <<endl;
+cout << Name_ans << ": ";
+getline(cin,date);
+cout << "Fahsai: "<<date <<"....that is OK!!! I'm looking forward to watching " << movie << " with you." <<endl;
+cout << Name_ans << ": ";
+getline(cin,goodbyetext);
+cout << "Fahsai: 555+ see you "<< date<< ". Bye Bye \\(^ ^)/";
+return 0;
+
+}
